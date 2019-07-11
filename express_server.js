ce442eacb33833c75f8694e1843ffa01d89be39a
Scott Appleton <Scottappleton09@gmail.com>
2019-07-11 20:32:58 +0000

    users can now only see the urls they created. they can also only delete their own urls

--- express_server.js
+++ express_server.js
@@ -3,35 +3,28 @@ const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
-const randomID = (length) => {  
+const randomID = (length) => {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for(var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
-
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
-
 
 const users = {
-  "testuserid" : {
-    id: 123,
-    email: "testuser@test.com",
-    password: "password123"
-  }
-};
 
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -47,56 +40,69 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); 
+});
 
 app.get("/urls", (request, response) => {
-  let templateVars = { urls: urlDatabase , 
-    "users": users, user_id: request.cookies.user_id};
+  let templateVars = {
+    urls: urlDatabase,
+    "users": users, user_id: request.cookies.user_id
+  };
+  if (!request.cookies.user_id) {
+    response.redirect("/login");
+  }
   response.render("urls_index", templateVars);
 });
 
 app.get("/login", (request, response) => {
-  let templateVars = { urls: urlDatabase , 
-    "users": users, user_id: request.cookies.user_id};
- response.render("login", templateVars);
+  let templateVars = {
+    urls: urlDatabase,
+    "users": users, user_id: request.cookies.user_id
+  };
+  response.render("login", templateVars);
 });
 
 app.get("/urls/new", (request, response) => {
-  let templateVars = { urls: urlDatabase , 
-    users: users, user_id: request.cookies.user_id};
-    if (!request.cookies.user_id) {
-      response.redirect("/urls");
-    }
+  let templateVars = {
+    urls: urlDatabase,
+    users: users, user_id: request.cookies.user_id
+  };
+  if (!request.cookies.user_id) {
+    response.redirect("/urls");
+  }
   response.render("urls_new", templateVars);
 });
 
 app.get("/registration", (request, response) => {
-  let templateVars = { urls: urlDatabase , 
-    users: users, user_id: request.cookies.user_id};
-  response.render("registration", templateVars);  
+  let templateVars = {
+    urls: urlDatabase,
+    users: users, user_id: request.cookies.user_id
+  };
+  response.render("registration", templateVars);
 });
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (request, response) => {
   // console.log(req.body);  // Log the POST request body to the console
 
   let newShortURL = randomID(6);
-  urlDatabase[newShortURL] = req.body.longURL;
-  res.redirect('/urls');
+  urlDatabase[newShortURL] = {};
+  urlDatabase[newShortURL].longURL = request.body.longURL;
+  urlDatabase[newShortURL].userID = request.cookies.user_id;
+  response.redirect("/urls");
 });
 
 app.post("/registration", (request, response) => {
   let newRandomID = randomID(10);
   for (const key in users) {
-    if (users[key].email === request.body.email){
+    if (users[key].email === request.body.email) {
       response.send("can't use that email");
       return;
     }
   }
-    users[newRandomID] = {};
-    users[newRandomID].id = newRandomID;
-    users[newRandomID].email = request.body.email;
-    users[newRandomID].password = request.body.password;
-    response.redirect("/urls");
+  users[newRandomID] = {};
+  users[newRandomID].id = newRandomID;
+  users[newRandomID].email = request.body.email;
+  users[newRandomID].password = request.body.password;
+  response.redirect("/urls");
 });
 
 app.post("/login", (request, response) => {
@@ -105,23 +111,28 @@ app.post("/login", (request, response) => {
       response.cookie('user_id', key);
       response.redirect("/urls");
       return;
-    } 
+    }
   }
   response.status(403).send("Invalid email or password");
-});
-  
-app.post("/urls/:shortURL/delete", (request, response) => {
-  delete urlDatabase[request.params.shortURL];
-  response.redirect('/urls');
+  response.redirect("/urls");
 });
 
+app.post("/urls/:shortURL/delete", (request, response) => {
+    if (urlDatabase[request.params.shortURL].userID === request.cookies.user_id) {
+      delete urlDatabase[request.params.shortURL];
+      response.redirect('/urls');
+    } else {
+      response.send("You may only delete urls created by you!")
+    }
+  }
+);
+
 app.post("/urls/:shortURL/update", (request, response) => {
-  
   response.redirect(`/urls/` + request.params.shortURL);
 });
 
 app.post("/urls/:shortURL/newLong", (request, response) => {
-  urlDatabase[request.params.shortURL] = request.body.newurl;
+  urlDatabase[request.params.shortURL].longURL = request.body.newurl;
   response.redirect("/urls");
 });
 
@@ -136,11 +147,14 @@ app.post("/logout", (request, response) => {
 });
 
 app.get("/urls/:id", (request, response) => {
-  let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id], users: users, user_id: request.cookies.user_id};
+  let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id].longURL, users: users, user_id: request.cookies.user_id };
   response.render("urls_show", templateVars);
 });
 
 app.get("/u/:firstParam", (req, res) => {
-  const longURL = urlDatabase[req.params.firstParam];
-  res.redirect(longURL);
+  if (urlDatabase[req.params.firstParam]) {
+    const longURL = urlDatabase[req.params.firstParam].longURL;
+    res.redirect(longURL);
+  }
+  
 });
